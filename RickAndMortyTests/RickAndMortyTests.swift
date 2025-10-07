953e970ac221b8b8ad2aa57e3d3f682befa09f83
Дмитрий Косик <d.kos@kode.ru>
2025-10-07 04:06:46 +0300

    Initial Commit

---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Dima Kosik on 7.10.25.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
